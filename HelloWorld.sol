9023d75985b572091e1590fa60930257f3dd53fb
Rob Fournier <78333324+robfjr@users.noreply.github.com>
2022-04-26 08:55:33 -0500

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// Simple Hello World Solidity smart contract
+/* For fast results, run this code in the
+   remix.ethereum.org IDE. */
+
+pragma solidity 0.8.7;
+
+contract HelloWorld {
+
+    string public happyGreeting = "Hello World!";
+    string public sadFarewell = "Goodbye, cruel world!";
+
+}
